b4550b1f4aa75bb2f26ba9fdf4e262f71448bee4
zzzghm <zhugehm@126.com>
2018-03-19 15:57:47 -0500

    Add files via upload

---
+++ Course Project/result.py
@@ -0,0 +1,141 @@
+Python 2.7.10 (default, May 23 2015, 09:44:00) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+... building the model
+... training
+('training @ iter = ', 0L)
+epoch 1, minibatch 8/8, validation error 95.000000 %
+     epoch 1, minibatch 8/8, test error of best model 92.500000 %
+epoch 2, minibatch 8/8, validation error 95.000000 %
+epoch 3, minibatch 8/8, validation error 95.000000 %
+epoch 4, minibatch 8/8, validation error 92.500000 %
+     epoch 4, minibatch 8/8, test error of best model 92.500000 %
+epoch 5, minibatch 8/8, validation error 95.000000 %
+epoch 6, minibatch 8/8, validation error 92.500000 %
+epoch 7, minibatch 8/8, validation error 90.000000 %
+     epoch 7, minibatch 8/8, test error of best model 90.000000 %
+epoch 8, minibatch 8/8, validation error 95.000000 %
+epoch 9, minibatch 8/8, validation error 95.000000 %
+epoch 10, minibatch 8/8, validation error 92.500000 %
+epoch 11, minibatch 8/8, validation error 92.500000 %
+epoch 12, minibatch 8/8, validation error 92.500000 %
+('training @ iter = ', 100L)
+epoch 13, minibatch 8/8, validation error 92.500000 %
+epoch 14, minibatch 8/8, validation error 92.500000 %
+epoch 15, minibatch 8/8, validation error 85.000000 %
+     epoch 15, minibatch 8/8, test error of best model 90.000000 %
+epoch 16, minibatch 8/8, validation error 92.500000 %
+epoch 17, minibatch 8/8, validation error 87.500000 %
+epoch 18, minibatch 8/8, validation error 82.500000 %
+     epoch 18, minibatch 8/8, test error of best model 85.000000 %
+epoch 19, minibatch 8/8, validation error 80.000000 %
+     epoch 19, minibatch 8/8, test error of best model 82.500000 %
+epoch 20, minibatch 8/8, validation error 87.500000 %
+epoch 21, minibatch 8/8, validation error 67.500000 %
+     epoch 21, minibatch 8/8, test error of best model 72.500000 %
+epoch 22, minibatch 8/8, validation error 42.500000 %
+     epoch 22, minibatch 8/8, test error of best model 55.000000 %
+epoch 23, minibatch 8/8, validation error 57.500000 %
+epoch 24, minibatch 8/8, validation error 42.500000 %
+epoch 25, minibatch 8/8, validation error 32.500000 %
+     epoch 25, minibatch 8/8, test error of best model 40.000000 %
+('training @ iter = ', 200L)
+epoch 26, minibatch 8/8, validation error 25.000000 %
+     epoch 26, minibatch 8/8, test error of best model 25.000000 %
+epoch 27, minibatch 8/8, validation error 20.000000 %
+     epoch 27, minibatch 8/8, test error of best model 20.000000 %
+epoch 28, minibatch 8/8, validation error 15.000000 %
+     epoch 28, minibatch 8/8, test error of best model 15.000000 %
+epoch 29, minibatch 8/8, validation error 10.000000 %
+     epoch 29, minibatch 8/8, test error of best model 10.000000 %
+epoch 30, minibatch 8/8, validation error 7.500000 %
+     epoch 30, minibatch 8/8, test error of best model 10.000000 %
+epoch 31, minibatch 8/8, validation error 7.500000 %
+epoch 32, minibatch 8/8, validation error 7.500000 %
+epoch 33, minibatch 8/8, validation error 7.500000 %
+epoch 34, minibatch 8/8, validation error 7.500000 %
+epoch 35, minibatch 8/8, validation error 7.500000 %
+epoch 36, minibatch 8/8, validation error 7.500000 %
+epoch 37, minibatch 8/8, validation error 7.500000 %
+('training @ iter = ', 300L)
+epoch 38, minibatch 8/8, validation error 5.000000 %
+     epoch 38, minibatch 8/8, test error of best model 7.500000 %
+epoch 39, minibatch 8/8, validation error 5.000000 %
+epoch 40, minibatch 8/8, validation error 5.000000 %
+epoch 41, minibatch 8/8, validation error 5.000000 %
+epoch 42, minibatch 8/8, validation error 5.000000 %
+epoch 43, minibatch 8/8, validation error 5.000000 %
+epoch 44, minibatch 8/8, validation error 5.000000 %
+epoch 45, minibatch 8/8, validation error 5.000000 %
+epoch 46, minibatch 8/8, validation error 5.000000 %
+epoch 47, minibatch 8/8, validation error 5.000000 %
+epoch 48, minibatch 8/8, validation error 5.000000 %
+epoch 49, minibatch 8/8, validation error 5.000000 %
+epoch 50, minibatch 8/8, validation error 5.000000 %
+('training @ iter = ', 400L)
+epoch 51, minibatch 8/8, validation error 5.000000 %
+epoch 52, minibatch 8/8, validation error 5.000000 %
+epoch 53, minibatch 8/8, validation error 5.000000 %
+epoch 54, minibatch 8/8, validation error 5.000000 %
+epoch 55, minibatch 8/8, validation error 5.000000 %
+epoch 56, minibatch 8/8, validation error 5.000000 %
+epoch 57, minibatch 8/8, validation error 5.000000 %
+epoch 58, minibatch 8/8, validation error 5.000000 %
+epoch 59, minibatch 8/8, validation error 5.000000 %
+epoch 60, minibatch 8/8, validation error 5.000000 %
+epoch 61, minibatch 8/8, validation error 5.000000 %
+epoch 62, minibatch 8/8, validation error 5.000000 %
+('training @ iter = ', 500L)
+epoch 63, minibatch 8/8, validation error 5.000000 %
+epoch 64, minibatch 8/8, validation error 5.000000 %
+epoch 65, minibatch 8/8, validation error 5.000000 %
+epoch 66, minibatch 8/8, validation error 5.000000 %
+epoch 67, minibatch 8/8, validation error 5.000000 %
+epoch 68, minibatch 8/8, validation error 5.000000 %
+epoch 69, minibatch 8/8, validation error 5.000000 %
+epoch 70, minibatch 8/8, validation error 5.000000 %
+epoch 71, minibatch 8/8, validation error 5.000000 %
+epoch 72, minibatch 8/8, validation error 5.000000 %
+epoch 73, minibatch 8/8, validation error 5.000000 %
+epoch 74, minibatch 8/8, validation error 5.000000 %
+epoch 75, minibatch 8/8, validation error 5.000000 %
+('training @ iter = ', 600L)
+epoch 76, minibatch 8/8, validation error 5.000000 %
+epoch 77, minibatch 8/8, validation error 5.000000 %
+epoch 78, minibatch 8/8, validation error 5.000000 %
+epoch 79, minibatch 8/8, validation error 5.000000 %
+epoch 80, minibatch 8/8, validation error 5.000000 %
+epoch 81, minibatch 8/8, validation error 5.000000 %
+epoch 82, minibatch 8/8, validation error 5.000000 %
+epoch 83, minibatch 8/8, validation error 5.000000 %
+epoch 84, minibatch 8/8, validation error 5.000000 %
+epoch 85, minibatch 8/8, validation error 5.000000 %
+epoch 86, minibatch 8/8, validation error 5.000000 %
+epoch 87, minibatch 8/8, validation error 5.000000 %
+('training @ iter = ', 700L)
+epoch 88, minibatch 8/8, validation error 5.000000 %
+epoch 89, minibatch 8/8, validation error 5.000000 %
+epoch 90, minibatch 8/8, validation error 5.000000 %
+epoch 91, minibatch 8/8, validation error 5.000000 %
+epoch 92, minibatch 8/8, validation error 5.000000 %
+epoch 93, minibatch 8/8, validation error 5.000000 %
+epoch 94, minibatch 8/8, validation error 5.000000 %
+epoch 95, minibatch 8/8, validation error 5.000000 %
+epoch 96, minibatch 8/8, validation error 5.000000 %
+epoch 97, minibatch 8/8, validation error 5.000000 %
+epoch 98, minibatch 8/8, validation error 5.000000 %
+epoch 99, minibatch 8/8, validation error 5.000000 %
+epoch 100, minibatch 8/8, validation error 5.000000 %
+('training @ iter = ', 800L)
+Optimization complete.
+Best validation score of 5.000000 % obtained at iteration 304, with test performance 7.500000 %
+The code for file train_CNN_olivettifaces.py ran for 2.17m
+>>> ================================ RESTART ================================
+>>> 
+picture: 89 is person 8, but mis-predicted as person 34
+picture: 178 is person 17, but mis-predicted as person 37
+picture: 189 is person 18, but mis-predicted as person 6
+picture: 299 is person 29, but mis-predicted as person 39
+picture: 368 is person 36, but mis-predicted as person 20
+>>> 
